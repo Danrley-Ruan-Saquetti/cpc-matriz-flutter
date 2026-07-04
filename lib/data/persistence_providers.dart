69c287fdf6d3e4cdf7e0ea47ca4331103870a0e3
Dan Ruan <danrsaquetti@gmail.com>
2026-07-04 14:08:32 -0300

    feat: Adaptando provider de repositórios para carregar a implementação baseada em `PersistenceConfig`

--- lib/data/persistence_providers.dart
+++ lib/data/persistence_providers.dart
@@ -1,3 +1,4 @@
+import 'package:cpc_matriz/core/config/persistence_config.dart';
 import 'package:cpc_matriz/data/repositories/item_repository.dart';
 import 'package:cpc_matriz/data/repositories/memory/in_memory_item_repository.dart';
 import 'package:cpc_matriz/data/repositories/memory/in_memory_movimentacao_repository.dart';
@@ -7,16 +8,26 @@ import 'package:cpc_matriz/data/repositories/movimentacao_repository.dart';
 import 'package:cpc_matriz/data/repositories/postgres/postgres_item_repository.dart';
 import 'package:cpc_matriz/data/repositories/postgres/postgres_movimentacao_repository.dart';
 import 'package:cpc_matriz/data/repositories/postgres/postgres_ticket_repository.dart';
+import 'package:cpc_matriz/data/repositories/sqlite/sqlite_item_repository.dart';
+import 'package:cpc_matriz/data/repositories/sqlite/sqlite_movimentacao_repository.dart';
+import 'package:cpc_matriz/data/repositories/sqlite/sqlite_ticket_repository.dart';
 import 'package:cpc_matriz/data/repositories/ticket_repository.dart';
 import 'package:cpc_matriz/data/services/postgresql_database_service.dart';
+import 'package:cpc_matriz/data/services/sqlite_database_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:nested/nested.dart';
 import 'package:provider/provider.dart';
 
 List<SingleChildWidget> buildPersistenceProviders() {
-  if (kIsWeb) return _memoryProviders();
+  if (kIsWeb) {
+    return _memoryProviders();
+  }
 
-  return _postgresProviders();
+  return switch (PersistenceConfig.source) {
+    PersistenceSource.memory => _memoryProviders(),
+    PersistenceSource.sqlite => _sqlitePersistenceProviders(),
+    PersistenceSource.postgres => _postgresProviders(),
+  };
 }
 
 List<SingleChildWidget> _memoryProviders() {
@@ -30,6 +41,24 @@ List<SingleChildWidget> _memoryProviders() {
     ),
     ProxyProvider<InMemoryStore, TicketRepository>(
       update: (_, store, _) => InMemoryTicketRepository(store),
+    ),
+  ];
+}
+
+List<SingleChildWidget> _sqlitePersistenceProviders() {
+  return [
+    Provider<SqliteDatabaseService>(
+      create: (_) => SqliteDatabaseService(),
+      dispose: (_, service) => service.dispose(),
+    ),
+    ProxyProvider<SqliteDatabaseService, ItemRepository>(
+      update: (_, service, _) => SqliteItemRepository(service),
+    ),
+    ProxyProvider<SqliteDatabaseService, MovimentacaoRepository>(
+      update: (_, service, _) => SqliteMovimentacaoRepository(service),
+    ),
+    ProxyProvider<SqliteDatabaseService, TicketRepository>(
+      update: (_, service, _) => SqliteTicketRepository(service),
     ),
   ];
 }
